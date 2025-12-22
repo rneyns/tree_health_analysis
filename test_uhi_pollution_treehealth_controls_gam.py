30b954d2275247fec7a4d6a4012515fa3c727e4f
rneyns <56020455+rneyns@users.noreply.github.com>
2025-12-22 10:16:30 +0100

    Update test_uhi_pollution_treehealth_controls_gam.py

--- test_uhi_pollution_treehealth_controls_gam.py
+++ test_uhi_pollution_treehealth_controls_gam.py
@@ -44,7 +44,7 @@ from scipy.stats import normaltest, shapiro, probplot
 # USER SETTINGS
 # ----------------------------
 
-RAW_DATA_CSV = "/Users/robbe_neyns/Documents/Work_local/research/UHI tree health/Data analysis/ndvi background investigations/ndvi_metrics_with_impervious.csv"  
+RAW_DATA_CSV = "/Users/robbe_neyns/Documents/Work_local/research/UHI tree health/Data analysis/ndvi background investigations/ndvi_metrics_with_impervious.csv"
 OUT_DIR = '/Users/robbe_neyns/Documents/Work_local/research/UHI tree health/Data analysis/ndvi background investigations'
 
 # Controls (always used)
